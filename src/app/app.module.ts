aec55c57b4ab7f442e40cb7e85463266582e5293
DheerajRaikwar <dheerajraikwarindian@gmail.com>
2023-01-20 19:28:50 +0530

    Added slide Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SlideListComponent } from './slide-show/slide-list/slide-list.component';
+import { SlideShowComponent } from './slide-show/slide-show.component';
+import { SlidePresentorComponent } from './slide-show/slide-presentor/slide-presentor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SlideListComponent,
+    SlideShowComponent,
+    SlidePresentorComponent
   ],
   imports: [
     BrowserModule
