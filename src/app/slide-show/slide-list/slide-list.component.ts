816d1c17c78f71e0b9e4741e591b016582106e04
DheerajRaikwar <dheerajraikwarindian@gmail.com>
2023-03-26 15:08:44 +0530

    new slide action

--- src/app/slide-show/slide-list/slide-list.component.ts
+++ src/app/slide-show/slide-list/slide-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Slide } from '../slide-model/slide-model';
+import { SlideShowComponent } from '../slide-show.component';
 
 @Component({
   selector: 'app-slide-list',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class SlideListComponent {
 
+  slides :Slide[]=[
+    new Slide("myView","This is a normal application",1)
+  ]
+
+  newSlide(){
+    this.slides.push(new Slide("dumy","dumy",0))
+  }
+  ngOnInit(){
+
+  }
 }
